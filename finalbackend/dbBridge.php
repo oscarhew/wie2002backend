0d43d082fa38fc5181d411c67430aaab318c58d2
oscarhew <75295323+oscarhew@users.noreply.github.com>
2023-06-08 21:51:33 +0800

    Add files via upload

---
+++ finalbackend/dbBridge.php
@@ -0,0 +1,30 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+
+include 'db_connect.php'; // Include the database connection file
+
+// Fetch data from the database
+$sql = "SELECT * FROM faq"; // Replace with your table name
+$result = $conn->query($sql);
+
+$data = array();
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $active = $row['active'];
+        if($active == '1')
+            $row['active'] = true;
+        else
+            $row['active'] = false;
+        $data[] = $row;
+    }
+}
+
+// Return data as JSON response
+header('Content-Type: application/json');
+echo json_encode($data);
+
+// Close the database connection
+$conn->close();
+?>
---
+++ finalbackend/db_connect.php
@@ -0,0 +1,22 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+
+$servername = "localhost"; // Replace with your MySQL server name
+$username = "root"; // Replace with your MySQL username
+$password = ""; // Replace with your MySQL password
+$dbname = "edufree"; // Replace with your MySQL database name
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// echo "Connected successfully";
+
+
+?>
