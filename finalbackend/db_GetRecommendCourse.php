681522c6e1d063700eff1e218e43bd9e78eb2326
oscarhew <xuan.hdx@gmail.com>
2023-06-17 11:48:50 +0800

    added getrecommendcourse api

--- finalbackend/db_GetRecommendCourse.php
+++ finalbackend/db_GetRecommendCourse.php
@@ -6,22 +6,31 @@ header("Access-Control-Allow-Headers: Content-Type");
 include 'db_connect.php'; // Include the database connection file
 
 // Fetch data from the database
-$sql = "SELECT * FROM faq"; // Replace with your table name
+$sql = "SELECT 
+    course.id as id,
+    course.image as `image`, 
+    author.name as `author`, 
+    course.rate as rate, 
+    course.title as title, 
+    course.price as price, 
+    course.rateCount as rateCount 
+FROM course INNER JOIN author ON author.ID = course.authorID ORDER BY course.rateCount DESC";
 $result = $conn->query($sql);
 
 $data = array();
 if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
-        $active = $row['active'];
-        $row['active'] = true;
         $data[] = $row;
     }
 }
 
-// Return data as JSON response
-header('Content-Type: application/json');
+
 echo json_encode($data);
+
+
+// Return data as JSON response
+// header('Content-Type: application/json');
+// echo json_encode($subCategoryArr);
 
 // Close the database connection
 $conn->close();
-?>
