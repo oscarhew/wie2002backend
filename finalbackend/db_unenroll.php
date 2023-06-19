6366ed0c181578a9e88f1017d070d63d2da851cd
oscarhew <xuan.hdx@gmail.com>
2023-06-19 20:38:42 +0800

    add unenroll

---
+++ finalbackend/db_unenroll.php
@@ -0,0 +1,32 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+
+include 'db_connect.php'; // Include the database connection file
+
+$postData = file_get_contents('php://input');
+$param = json_decode($postData, true);
+$userId = $param['userId'];
+$courseId = $param['courseId'];
+
+// Fetch data from the database
+$sql = "DELETE FROM enrollment WHERE enrollment.userId = " . $userId. " AND enrollment.courseId=". $courseId;
+
+$result = $conn->query($sql);
+
+$data = array();
+if ($result->num_rows == 0) {
+    echo json_encode('true');
+}else{
+    echo json_encode('false');
+}
+
+
+
+// Return data as JSON response
+// header('Content-Type: application/json');
+// echo json_encode($subCategoryArr);
+
+// Close the database connection
+$conn->close();
